cdbeddbb4f57850e8a52f9db803ab65c77c3f54c
charlybarrio <5757@holbertonstudents.com>
2023-04-02 15:50:58 -0700

    Pycodstyle

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -21,9 +21,8 @@ if __name__ == "__main__":
     with open(filename, mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow(
-                    [employee_id, employee['username'], task['completed']
-                        task['title']])
+            writer.writerow([employee_id, employee['username'],
+                            task['completed'], task['title']])
 
     print("Employee {} is done with tasks({}/{}):".format(employee.get('name'),
           len([t for t in todos if t['completed']]), len(todos)))
